a4f68400054ceb1f76d90c5ee072e81a1f098df6
lexloo <lexloo@126.com>
2015-01-04 21:06:28 +0800

    first commit

---
+++ src/plugins/nv.parser.js
@@ -0,0 +1,52 @@
+/**
+ * parser - NvUI
+ *
+ * Copyright(c) 2014 lexloo [ lexloo@nv-app.com ]
+ *
+ */
+
+(function($) {
+	$.parser = {
+		auto: true,
+		onComplete: function(context) {},
+		plugins: [],
+		parse: function(context) {
+			var aa = [];
+			for (var i = 0; i < $.parser.plugins.length; i++) {
+				var name = $.parser.plugins[i];
+				var r = $('.nv-' + name, context);
+				if (r.length) {
+					if (r[name]) {
+						r[name]();
+					} else {
+						aa.push({
+							name: name,
+							jq: r
+						});
+					}
+				}
+			}
+			if (aa.length && window.loader) {
+				var names = [];
+				for (var i = 0; i < aa.length; i++) {
+					names.push(aa[i].name);
+				}
+				loader.load(names, function() {
+					for (var i = 0; i < aa.length; i++) {
+						var name = aa[i].name;
+						var jq = aa[i].jq;
+						jq[name]();
+					}
+					$.parser.onComplete.call($.parser, context);
+				});
+			} else {
+				$.parser.onComplete.call($.parser, context);
+			}
+		}
+	};
+	$(function() {
+		if (!window.loader && $.parser.auto) {
+			$.parser.parse();
+		}
+	});
+})(jQuery);
